8175f5650976381836c8b2c0b67d00667f77abca
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-08-09 13:54:17 +0200

    collect temperature from mqtt and send it to fp

---
+++ WiringPi/temp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+killall fp || true
+
+# kill me and all children on ctrl+c
+trap "trap - TERM && kill -- -$$" INT TERM EXIT
+
+./fp &
+sleep 1
+pid=$!
+echo "fp pid: $pid"
+
+mosquitto_sub -h rpi2 -t 'stat/boiler/DS18B20/temperature' | while read temp ; do
+	a=$( echo $temp | cut -d\. -f1 )
+	b=$( echo $temp | cut -d\. -f2 )
+	echo "$a:$b"
+	echo "time_hour=$a\ntime_min=$b\n" > lcd.cfg
+	kill -USR1 $pid
+	sleep 1
+done
